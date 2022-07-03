2c9a7fcade6a97b8e4ecb85382db401cecaced40
juliana <julianacmenezes@msn.com>
2022-07-02 21:06:42 -0400

    practica JS

---
+++ JS/script.js
@@ -0,0 +1,94 @@
+
+//Bootlet 01 homero
+/*let nombre = prompt ('Ingresa tu nombre');
+let apellido = prompt ('Ingresa tu apellido');
+let edad = parseInt (prompt ('Ingreda tu edad'));
+
+
+alert('Bienvenida(o) '+nombre+' '+apellido);
+*/
+
+
+//bootlet2 ciudades
+/*
+const cl = 'Chile';
+const br = 'Brasil';
+const usa = "Estados Unidos";
+const ita = 'Italia';
+const ca = 'Canada';
+console.log(cl);
+console.log(br);
+console.log(usa);
+console.log(ita);
+console.log(ca);
+*/
+
+//booklet3 carnet
+/*
+let nombreCarnet = nombre+' '+apellido;
+let direccionCarnet = prompt('Ingresa tu direccion');
+let pais = prompt ('Ingresa tu pais');
+let carnet = nombreCarnet+' '+direccionCarnet+' '+pais;
+alert ('Tu carnet es: '+carnet)
+*/
+
+//Bootlet3.2 carnetv2
+/*
+let nombre = prompt ('Ingresa tu nombre completo');
+let domicilio = prompt ('Ingresa tu domicilio');
+let pais = prompt ('Ingresa tu pais');
+let nascimiento = prompt ('Ingresa tu fecha de nascimiento');
+const codigocarnet = "w987w398739879";
+
+alert('Tu carnet de conducir es: '+'\n nombre \n domicilio \n pais \n nascimiento \n codigocarnet');
+*/
+
+//Bootlet 4 registro de personas
+/*
+let padre = prompt('Ingresa el nombre de su padre');
+let madre = prompt ('Ingresa el nombre de su madre');
+let hermano = prompt ('Ingresa el nombre de su hermana(o)');
+let abuela = prompt('Ingresa el nombre de su abuela');
+let pri = prompt('Ingresa el nombre de su prima');
+let familia = padre+' '+madre+' '+hermano+' '+abuela+' '+pri;
+
+alert ('Tu familia es: '+familia);
+*/
+
+//Bootlet5 descuentos
+/*
+let precio1 = parseFloat(prompt('Ingresa el precio de la carne'));
+let precio2 = parseFloat(prompt('Ingresa el precio del pescado'));
+let descuentocarne = precio1 - (precio1 * 0.2);
+let descuentopescado = precio2 - (precio2 * 0.3);
+
+alert (descuentocarne);
+alert (descuentopescado);
+*/
+
+//ACTIVIDAD1 YO FUI Yo
+/*
+let nombreUsuario = prompt('Cual es su nombre');
+let nombreOtrousuario = ('Juliana');
+
+if (nombreUsuario == nombreOtrousuario) {
+    alert ('Fui Yo')
+}else{
+    alert('No fui yo');
+}
+*/
+
+//ACTIVIDAD 1 PRESIONAR Y
+/*
+let teclaUsuario = prompt('Presionar cualqueir tecla');
+let teclaYminuscula = ('y');
+let teclaYmayuscula = ('Y');
+
+if ((teclaUsuario == teclaYminuscula) || (teclaUsuario == teclaYmayuscula)){
+    alert ('Correcto');
+}else {
+    alert ('Error');
+}
+teclaUsuario = prompt('Presionar cualqueir tecla');
+*/
+
